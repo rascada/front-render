5baa38ccd39cd636fe82e2d5186cd66335172482
Damian <carftjr@gmail.com>
2015-09-23 17:14:24 +0200

    storm reformat

--- index.js
+++ index.js
@@ -1,64 +1,64 @@
 'use strict';
 
 let render = require('./render.js'),
-	gui;
+    gui;
 
-switch(process.argv[2]){
-	case "watch":
-		render.log('watch mode');
-		render.watcher = true;
-		break;
-	case "gui":
-		render.log('gui mode')
-		render.watcher = true;
-		gui = true;
-		break;
-	default:
-		render.log('Just one compile');
-		break;
+switch (process.argv[2]) {
+    case "watch":
+        render.log('watch mode');
+        render.watcher = true;
+        break;
+    case "gui":
+        render.log('gui mode')
+        render.watcher = true;
+        gui = true;
+        break;
+    default:
+        render.log('Just one compile');
+        break;
 }
 
-if(gui){
-	let	fs = require('fs'),
-		path = require('path'),
-		express = require('express'),
-		app = express(),
-		server = require('http').createServer(app),
-		io = require('socket.io')(server);
+if (gui) {
+    let fs = require('fs'),
+        path = require('path'),
+        express = require('express'),
+        app = express(),
+        server = require('http').createServer(app),
+        io = require('socket.io')(server);
 
-	server.listen(80, function(){
-		render.log(`Serwer uruchomiony localhost:${server.address().port}`);
-	});
+    server.listen(80, function () {
+        render.log(`Serwer uruchomiony localhost:${server.address().port}`);
+    });
 
-	let dirTree = {};
-	fs.readdir(__dirname, function(err, files){
-		let isFolder = (files, prevFile, prevPer) =>{
-			files.forEach(function(file){
-				let dir = path.join(prevFile, file);
-				if( fs.statSync(dir).isDirectory() ){
-					if(!prevPer.hasOwnProperty(file)) prevPer[file] = {};
-					isFolder(fs.readdirSync(dir), dir, prevPer[file]);
-				}else
-					prevPer[file] = dir;
-			});
-		};
-		isFolder(files, __dirname, dirTree);
-	});
+    let dirTree = {};
+    fs.readdir(__dirname, function (err, files) {
+        let isFolder = (files, prevFile, prevPer) => {
+            files.forEach(function (file) {
+                let dir = path.join(prevFile, file);
+                if (fs.statSync(dir).isDirectory()) {
+                    if (!prevPer.hasOwnProperty(file)) prevPer[file] = {};
+                    isFolder(fs.readdirSync(dir), dir, prevPer[file]);
+                } else
+                    prevPer[file] = dir;
+            });
+        };
+        isFolder(files, __dirname, dirTree);
+    });
 
-	app.use(express.static('public'));
-	app.set('view engine', 'jade');
-	app.get('/', function(req, res){
-		res.render('index.jade', {
-			dir: dirTree
-		});
-	});
+    app.use(express.static('public'));
+    app.set('view engine', 'jade');
+    app.get('/', function (req, res) {
+        res.render('index.jade', {
+            dir: dirTree
+        });
+    });
 
-	io.on('connection', function (socket) {
-		render.log(`nawiązano połączenie ${socket.id}`);
-		render.socket = socket;
-		render.log('Witaj')
-		socket.emit('dirTree', {dirTree: dirTree});
-	});
+    io.on('connection', function (socket) {
+        render.log(`nawiązano połączenie ${socket.id}`);
+        render.socket = socket;
+        render.log('Witaj');
+        socket.emit('dirTree', {dirTree: dirTree});
+    });
 
 }
 render.log(`zapisuje logi do ${render.logs}`, true);
--- render.js
+++ render.js
@@ -1,24 +1,24 @@
 'use strict';
 
-let fs = require('fs'),   
+let fs = require('fs'),
     path = require('path'),
     jade = require('jade'),
-    stylus = require('stylus'), 
+    stylus = require('stylus'),
     babel = require('babel');
 
 module.exports = {
     logs: path.join(__dirname, 'main.log'),
     socket: null, watcher: false,
-    toRender: function(toRenderFiles){
+    toRender: function (toRenderFiles) {
         toRenderFiles.forEach((file)=> {
             this.watch(this[file[0]], file[1], file[2]);
         });
     },
     watch: function (engine, file, render) {
         engine.call(this, file, render);
-        if(this.watcher) fs.watchFile(file, () => engine.call(this, file, render));
+        if (this.watcher) fs.watchFile(file, () => engine.call(this, file, render));
     },
-    watchFolder: function(dir){
+    watchFolder: function (dir) {
         fs.watch(dir, function (event, filename) {
             console.log('event is: ' + event);
             if (filename) {
@@ -29,35 +29,35 @@ module.exports = {
         });
     },
     jade: function (jadeFile, html) {
-        fs.writeFile(html, jade.renderFile(jadeFile),(err) =>{
+        fs.writeFile(html, jade.renderFile(jadeFile), (err) => {
             if (err) this.log(err);
             this.log(`${html} skompilowany przez 'jade'`);
         });
     },
     stylus: function (styl, css) {
-        fs.readFile(styl,(err, file) => {
+        fs.readFile(styl, (err, file) => {
             if (err) this.log(err);
             fs.writeFile(css, stylus.render(file.toString()), () => this.log(`${css} skompilowany przez 'stylus'`));
         });
     },
-    babel: function(es6, js){
-       babel.transformFile(es6, (err, babel) => {
-            if(err) this.log(err);
-            fs.writeFile(js, babel.code,{
+    babel: function (es6, js) {
+        babel.transformFile(es6, (err, babel) => {
+            if (err) this.log(err);
+            fs.writeFile(js, babel.code, {
                 comments: false,
                 compact: true,
                 stage: 0
             }, (err) => {
-                if(err) this.log(err);
+                if (err) this.log(err);
                 this.log(`${js} skompilowany przez 'babel'`);
             });
         });
     },
-    log: function(message, consoleOnly){
-						message = `[${new Date().toLocaleString()}] [renderjs] ${message}`;
-						
-						console.log(message);
-                        if(this.socket) this.socket.emit('log',{log: message});
-						if(!consoleOnly && this.logs) fs.appendFile(this.logs, `${message}\n`);
+    log: function (message, consoleOnly) {
+        message = `[${new Date().toLocaleString()}] [renderjs] ${message}`;
+
+        console.log(message);
+        if (this.socket) this.socket.emit('log', {log: message});
+        if (!consoleOnly && this.logs) fs.appendFile(this.logs, `${message}\n`);
     }
 };
