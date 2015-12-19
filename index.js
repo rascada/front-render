7d17150cc807f04f503e2c502684492dbe52b383
Damian <carftjr@gmail.com>
2015-12-19 18:02:13 +0100

    jscs

--- index.js
+++ index.js
@@ -2,21 +2,21 @@
 
 'use strict';
 
-let render = require('./render.js'),
-    fs = require('fs');
+let render = require('./render.js');
+let fs = require('fs');
 
-if(process.argv[4]){ //when 3 arguments render one file - front-render stylus views/main.styl public/main.css
-    render[process.argv[2]](process.argv[3], process.argv[4]);
+if (process.argv[4]) { //when 3 arguments render one file - front-render stylus views/main.styl public/main.css
+  render[process.argv[2]](process.argv[3], process.argv[4]);
 }else {
-    switch (process.argv[2]) {
-        case "watch": // watch files from toRender.json
-            render.inspect(process.cwd(), true);
-            break;
-        case "-v":
-            render.log(render.version);
-            break;
-        default: // 0 arguments - one compile from toRender.json
-            render.renderJSON();
-            break;
-    }
+  switch (process.argv[2]) {
+    case 'watch': // watch files from toRender.json
+      render.inspect(process.cwd(), true);
+      break;
+    case '-v':
+      render.log(render.version);
+      break;
+    default: // 0 arguments - one compile from toRender.json
+      render.renderJSON();
+      break;
+  }
 }
--- test/render.js
+++ test/render.js
@@ -1,85 +1,78 @@
 'use strict';
 
-let fs = require('fs'),
-    tape = require('tape'),
-    fRender = require('../render'),
-    unlink = ext => fs.unlink( `test.${ext}` );
+let fs = require('fs');
+let tape = require('tape');
+let fRender = require('../render');
+let unlink = ext => fs.unlink(`test.${ext}`);
 
 fRender.logs = false;
 
 tape('stylus compilation engine', test => {
-    test.plan(1);
+  test.plan(1);
 
-    let styl = `
-        body
-            background red + 10%
-        `;
+  let styl = `
+      body
+          background red + 10%
+      `;
 
-    fs.writeFile('test.styl', styl, function(err){
+  fs.writeFile('test.styl', styl, function(err) {
+    if (err) test.fail(err);
+
+    fRender.stylus('test.styl', 'test.css', function(err, log) {
+      if (err) test.fail(err);
+
+      fs.readFile('test.css', function(err, file) {
 
         if (err) test.fail(err);
 
-        fRender.stylus('test.styl', 'test.css', function(err, log){
-
-            if(err) test.fail(err);
-
-            fs.readFile('test.css', function(err, file){
-
-                if(err) test.fail(err);
-
-                test.equal(file.toString(), 'body {\n  background: #ff1a1a;\n}\n', log.message);
-                ['css', 'styl'].forEach( unlink );
-            });
-        });
+        test.equal(file.toString(), 'body {\n  background: #ff1a1a;\n}\n', log.message);
+        ['css', 'styl'].forEach(unlink);
+      });
     });
+  });
 });
 
 tape('babel compilation engine', test => {
-    test.plan(1);
+  test.plan(1);
 
-    let es6 = `
-        let b = b => b <= 5;
-        `;
+  let es6 = `
+      let b = b => b <= 5;
+      `;
 
-    fs.writeFile('test.es6.js', es6, function(err){
+  fs.writeFile('test.es6.js', es6, function(err) {
 
+    if (err) test.fail(err);
+
+    fRender.babel('test.es6.js', 'test.js', function(err, log) {
+      if (err) test.fail(err);
+
+      fs.readFile('test.js', function(err, file) {
         if (err) test.fail(err);
 
-        fRender.babel('test.es6.js', 'test.js', function(err, log){
-
-            if(err) test.fail(err);
-
-            fs.readFile('test.js', function(err, file){
-
-                if(err) test.fail(err);
-
-                test.equal(file.toString(), '"use strict";\n\nvar b = function b(_b) {\n        return _b <= 5;\n};', log.message);
-                ['js', 'es6.js'].forEach( unlink );
-            });
-        });
+        test.equal(file.toString(), '"use strict";\n\nvar b = function b(_b) {\n        return _b <= 5;\n};', log.message);
+        ['js', 'es6.js'].forEach(unlink);
+      });
     });
+  });
 });
 
 tape('jade compilation engine', test => {
-    test.plan(1);
+  test.plan(1);
 
-    let jade = 'doctype html\nbody\n   h1 test';
+  let jade = 'doctype html\nbody\n   h1 test';
 
-    fs.writeFile('test.jade', jade, function(err){
+  fs.writeFile('test.jade', jade, function(err) {
+    if (err) test.fail(err);
 
+    fRender.jade('test.jade', 'test.html', function(err, log) {
+      if (err) test.fail(err);
+
+      fs.readFile('test.html', function(err, file) {
         if (err) test.fail(err);
 
-        fRender.jade('test.jade', 'test.html', function(err, log){
-
-            if(err) test.fail(err);
-
-            fs.readFile('test.html', function(err, file){
-
-                if(err) test.fail(err);
-
-                test.equal(file.toString(), '<!DOCTYPE html><body><h1>test</h1></body>', log.message);
-                ['jade', 'html'].forEach( unlink );
-            });
-        });
+        test.equal(file.toString(), '<!DOCTYPE html><body><h1>test</h1></body>', log.message);
+        ['jade', 'html'].forEach(unlink);
+      });
     });
+  });
 });
